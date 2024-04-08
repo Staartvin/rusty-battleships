2bc83b3dde9c8b26bcbb81c90811fa6c22befdc2
Vincent Bolta <mijnleraar@msn.com>
2024-04-08 16:17:41 +0200

    Add coordinates of battleship in struct

--- src/battleship.rs
+++ src/battleship.rs
@@ -1,6 +1,9 @@
+use crate::board::BoardPosition;
+
 pub struct Battleship {
     pub name: BattleshipName,
     pub size: u32,
+    pub coordinates: Vec<BoardPosition>,
 }
 
 #[derive(strum::EnumIter, Debug, strum::VariantArray, Clone, Copy)]
